576314910430fb65d9bbdc0497df9cf759367c1e
Albino1995 <763968024@qq.com>
2017-12-25 10:18:50 +0800

    update loading

---
+++ src/base/loading/loading.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="loading">
+    <img width="24" height="24" src="./loading.gif">
+    <p class="desc">{{title}}</p>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      title: {
+        type: String,
+        default: ''
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .loading
+    width: 100%
+    text-align: center
+    .desc
+      line-height: 20px
+      font-size: 12px
+      color: #9a9a9a
+</style>
